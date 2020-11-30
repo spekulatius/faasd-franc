af09491f2888f95f4ed28dcc0531af9fe5b86598
Peter Thaleikis <peter.thaleikis@gmail.com>
2020-11-30 21:28:57 +0400

    Implement basic functionality

--- handler.js
+++ handler.js
@@ -1,12 +1,20 @@
 'use strict'
 
+let franc = require('franc')
+
 module.exports = async (event, context) => {
-  const result = {
-    'status': 'Received input: ' + JSON.stringify(event.body)
+  let response = {}
+
+  if (event.query.query == undefined || event.query.query.length == 0) {
+    response.status = 'Error'
+    response.message = 'No query string provided'
+  } else {
+    response.status = 'Success'
+    response.result = franc.all(event.query.query)
   }
 
   return context
     .status(200)
-    .succeed(result)
+    .headers({"Content-Type": "application/json"})
+    .succeed(response)
 }
-
